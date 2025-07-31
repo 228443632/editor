8f44159f173d5eb49f5913d595d3da78414149f7
bianpengfei <228443632@qq.com>
2025-07-31 10:22:38 +0800

    perf: dev1.0 监听事件优化

--- src/composables/useEditorEvent.ts
+++ src/composables/useEditorEvent.ts
@@ -4,28 +4,41 @@
  * @create 23/04/25 AM12:03
  */
 import type { Editor } from '@tiptap/vue-3'
+import type { TCommonFunc } from 'sf-utils2/types/generic-helper'
 
 export function useEditorEvent(editor: Ref<Editor>) {
-  const events = ref({})
-  const addListenerEvent = (eventName: string, callback: Function) => {
-    events.value[eventName] = callback
+  const events = ref({}) as Ref<Record<string, TCommonFunc[]>>
+  const addListenerEvent = (eventName: string, callback: TCommonFunc) => {
+    if (!editor.value) return
+    events.value[eventName] ||= []
+    events.value[eventName].push(callback)
     editor.value.on(eventName, callback)
   }
 
-  const removeListenerEvent = (eventName: string, callback: Function) => {
+  const removeListenerEvent = (eventName: string, callback: TCommonFunc) => {
     editor.value.off(eventName, callback)
+  }
+
+  const removeListenerEventEntire = (eventName: string) => {
+    const cbs = events.value[eventName] as TCommonFunc[]
+    if (cbs.length) {
+      cbs.forEach((cb) => {
+        editor.value.off(eventName, cb)
+      })
+    }
   }
 
   onBeforeUnmount(() => {
     Object.keys(events.value).forEach((eventName) => {
+      removeListenerEventEntire(eventName)
       events.value[eventName] = null
-      removeListenerEvent(eventName, events.value[eventName])
     })
   })
 
   return {
     addListenerEvent,
     removeListenerEvent,
+    removeListenerEventEntire,
   }
 }
 
