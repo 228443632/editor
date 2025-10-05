fc36d2473aadfeebfb2143340ecae7a537bfee4c
bianpengfei <228443632@qq.com>
2025-10-05 09:23:31 +0800

    feat: 优化二期需求(10%)

---
+++ src/views/preview-result/components/ContentCompSignDate.vue
@@ -0,0 +1,54 @@
+<!--
+ * @Description: 签署日期控件
+ * @Author 卞鹏飞 <228443632@qq.com>
+ * @create 02/10/25 PM7:53
+ -->
+<!--setup-->
+<script setup lang="ts">
+import { COMP_SIGN_DATE_STYLE } from '@/views/doc-editor/extensions/constant.ts'
+import type { IParamsCompItem } from '@/views/preview-editor/types/types.ts'
+import testSignSvgRaw from '@/assets/images/test-sign.svg?raw'
+
+const { proxy } = getCurrentInstance()
+const props = defineProps({
+  /**
+   * 节点数据
+   */
+  nodeData: {
+    type: Object as PropType<IParamsCompItem>,
+    default: () => ({}),
+  },
+})
+const emit = defineEmits([])
+
+/* 状态 */
+
+/* 方法 */
+
+/* 计算 */
+
+/* 监听 */
+
+/* 周期 */
+onMounted(() => {})
+
+/* 暴露 */
+defineExpose({
+  $: proxy.$,
+})
+</script>
+
+<!--render-->
+<template>
+  <div
+    ref="divRef"
+    :style="{
+      width: COMP_SIGN_DATE_STYLE.width + 'px',
+      height: COMP_SIGN_DATE_STYLE.height + 'px',
+    }"
+    v-html="testSignSvgRaw"
+  ></div>
+</template>
+
+<!--style-->
+<style scoped lang="less"></style>
